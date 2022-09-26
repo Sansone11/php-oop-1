0088c130a62db5b11ac320cf3c348ddc2286a318
Sansone11 <sansonemirco@gmail.com>
2022-09-26 18:07:47 +0200

    card-edit-bug-fix

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>php-oop-1</title>
 </head>
 
@@ -14,14 +15,16 @@
     class movie
     {
         // interno della classe sono dichiarate delle variabili d’istanza
+        public $poster;
         public $title;
         public $year;
         public $regista;
         public $actor;
         public $vote;
         // all’interno della classe è definito un costruttore
-        function __construct($title, $year, $regista, $actor, $vote)
+        function __construct($poster, $title, $year, $regista, $actor, $vote)
         {
+            $this->poster = $poster;
             $this->title = $title;
             $this->year = $year;
             $this->regista = $regista;
@@ -42,69 +45,68 @@
         }
     }
 
-    $Rocky = new movie('Rocky', 1986, 'Silvester Stallone', 'Silvester Stallone', 6);
+    $Rocky = new movie('https://pad.mymovies.it/filmclub/2006/08/254/locandina.jpg', 'Rocky', 1986, 'Silvester Stallone', 'Silvester Stallone', 6);
     $Rocky->setVote(4);
     // var_dump($Rocky);
 
-    $IlPadrino = new movie('Il Padrino', 1972, 'Martin Scorsese', 'Al Pacino', 9,);
+    $IlPadrino = new movie('https://pad.mymovies.it/filmclub/2002/08/056/locandina.jpg', 'Il Padrino', 1972, 'Martin Scorsese', 'Al Pacino', 9,);
     $IlPadrino->setVote(18);
     // var_dump($IlPadrino);
 
-    $LoSqualo = new movie('Lo Squalo', 1975, 'Steven Spielberg', 'Roy Scheider', 8);
+    $LoSqualo = new movie('https://www.artovercovers.com/wp-content/uploads/2020/07/lo-squalo.jpg', 'Lo Squalo', 1975, 'Steven Spielberg', 'Roy Scheider', 8);
     $LoSqualo->setVote(-3);
     // var_dump($LoSqualo);
 
-    $Shining = new movie('Shining', 1980, 'Stanley Kubrik', 'Jack Nicholson', 8);
+    $Shining = new movie('http://cultura.biografieonline.it/wp-content/uploads/2012/09/shining-locandina.jpg', 'Shining', 1980, 'Stanley Kubrik', 'Jack Nicholson', 8);
     $Shining->setVote(4);
     // var_dump($Shining);
-    
+
     $movies = [
         $Rocky,
         $IlPadrino,
         $LoSqualo,
         $Shining,
     ];
-   
+
     ?>
-    <div class="Card_wrapper">
-        <div class="card">
-            <?php
-            foreach ($movies as $movie) {
-                // var_dump($movie);
-                ?>
+    <div class="card_wrapper">
+        <?php
+        foreach ($movies as $movie) {
+            // var_dump($movie);
+        ?>
+            <div class="card">
+                <img src="<?php
+                            echo $movie->poster;
+                            ?>" alt="">
+
                 <h1>
                     <?php
                     echo $movie->title;
                     ?>
                 </h1>
                 <p>
-                <?php
+                    <?php
                     echo $movie->year;
-                ?>
-                </p>
-                <p>
-                <?php
-                    echo $movie ->regista;
-                ?>
-                </p>
-                <p>
-                    <?php 
-                    echo $movie->actor;
                     ?>
                 </p>
                 <p>
-                <?php
-                echo $movie->vote;
-                ?>
+                    <?php
+                    echo $movie->regista;
+                    ?>
                 </p>
-                <?php
-            };
-            ?>
-
-            
-
-        </div>
-
+                <p>
+                    <?php
+                    echo $movie->actor;
+                    ?>
+                </p>
+                <p class="vote">
+                    <?php
+                    echo $movie->vote;
+                    ?>
+                </p>
+            </div><?php
+                };
+                    ?>
     </div>
     <h1></h1>
 
